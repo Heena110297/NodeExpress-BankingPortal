a72a5a8acf64cd827d5eac9f9f99e11fde4fae98
Heena110297 <44188323+Heena110297@users.noreply.github.com>
2018-11-14 15:30:51 +0530

    corrected

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');//core lirary
 const app = express();
 const {accounts , users , writeJSON}= require('./data');
 const accountRoutes = require('./routes/accounts');
-const serviceRoutes = require('./routes/services');
+const servicesRoutes = require('./routes/services');
 app.set('views' ,path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));//to point at public directory
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}));
 //express middleware to handle post data
 //All of our CSS/JS for the client-side is found in the public directory. We need to point express to public.
 app.use('/account',accountRoutes);
-app.use('/services',serviceRoutes);
+app.use('/services',servicesRoutes);
 
 
 
